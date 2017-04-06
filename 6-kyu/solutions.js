918a9163c2df5b6838da2d3e3de353e76bf21193
Hieu Nguyen <hieutnguyen28@gmail.com>
2017-04-05 23:15:32 -0700

    added more 6-kyu solution

--- 6-kyu/solutions.js
+++ 6-kyu/solutions.js
@@ -71,3 +71,22 @@ function comp(array1, array2){
     return sqArr.indexOf('false') !== -1 ? false:true;  
   }
 }
+
+
+// Challenge:
+// Replace With Alphabet Position
+
+// Welcome. In this kata you are required to, given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it. a being 1, b being 2, etc. As an example:
+
+// alphabet_position("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (As a string.)
+
+// Solution:
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  var str = '';
+  text.split('').map((letter)=>{
+    str += alphabet.indexOf(letter.toLowerCase()) === -1 ? '': (alphabet.indexOf(letter.toLowerCase()) + 1)+' ';
+    });
+ return str.trim();
+}
